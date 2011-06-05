3a1c990180fae3ccd25726726795f86e1e93d581
Bernhard Hofmann <bhofmann@hotmail.co.uk>
2011-06-05 13:08:52 +0100

    Still have issues with the DSL and app sequence on startup - sometimes the event listeners just don't register.

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -1,15 +1,8 @@
 // the app method accepts a fn to invoke on init unobtrusively 
-var run = function(application) {
-    if ((navigator.userAgent.indexOf('Browzr') > -1) || navigator.platform === 'Win32') {
-        // blackberry
-        setTimeout(application, 250);
-    } else {
-		// attach to deviceready event, which is fired when phonegap is all good to go.
-		x$(document).on('deviceready', application, false);
-	}
+var run = function(onDeviceReady) {
+	// attach to deviceready event, which is fired when phonegap is all good to go.
+	document.addEventListener("deviceready", onDeviceReady, false);
 }
-
-, isReady = false
 
 // shows id passed
 , display = function(id) {
