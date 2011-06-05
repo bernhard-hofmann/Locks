c4f8554b0f4f708b08990dbfea4d279b47099b38
Bernhard Hofmann <bhofmann@hotmail.co.uk>
2011-06-05 11:27:21 +0100

    Reverting to the DSL and tiny controller for handling button touches.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,43 +1,44 @@
-var activity = 'click';
-if ('ontouchstart' in document.documentElement) {
-	activity = 'touchstart';
-};
+document.addEventListener("deviceready", phoneGapReady, false);
 
-// immediately invoked on first run
-var init = (function () {
-})();
+function phoneGapReady() {
+	var activity = 'click';
+	if ('ontouchstart' in document.documentElement) {
+		activity = 'touchstart';
+	};
 
-// a little inline controller
-//when('#welcome');
-when('#play', function () {
-	display('#play');
-});
-when('.home', function () {
-	display('#welcome');
-});
-x$('#reset').on(activity, function() {
-	resetLocks();
-});
+	// a little inline controller
+	when('#play', function () {
+		display('#play');
+	});
+	when('.welcome', function () {
+		display('#welcome');
+	});
+	x$('#reset').on(activity, function() {
+		resetLocks();
+	});
 
-/*
-document.addEventListener("backbutton", onBackKeyDown, false);
-document.addEventListener("menubutton", onMenuKeyDown, false);
-document.addEventListener("pause", onPause, false);
-document.addEventListener("resume", onResume, false);
-document.addEventListener("searchbutton", onSearchKeyDown, false);
-function onBackKeyDown() {
-	alert('Back button not supported.');
+	document.addEventListener("menubutton", onMenuKeyDown, false);
+	function onMenuKeyDown() {
+		display('#welcome');
+	}
+
+	document.addEventListener("searchbutton", onSearchKeyDown, false);
+	function onSearchKeyDown() {
+		display('#deviceInfo');
+	}
+	/*
+	document.addEventListener("backbutton", onBackKeyDown, false);
+	document.addEventListener("menubutton", onMenuKeyDown, false);
+	document.addEventListener("pause", onPause, false);
+	document.addEventListener("resume", onResume, false);
+	function onBackKeyDown() {
+		alert('Back button not supported.');
+	}
+	function onPause() {
+		// Handle the pause event
+	}
+	function onResume() {
+		alert('Welcome back! :D');
+	}
+	*/
 }
-function onMenuKeyDown() {
-	display('#welcome');
-}
-function onPause() {
-	// Handle the pause event
-}
-function onResume() {
-	alert('Welcome back! :D');
-}
-function onSearchKeyDown() {
-	display('#deviceInfo');
-}
-*/
--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -1,26 +1,13 @@
-// the app method accepts a fn to invoke on init unobtrusively 
-var run = function(application) {
-    if ((navigator.userAgent.indexOf('Browzr') > -1) || navigator.platform === 'Win32') {
-        // blackberry
-        setTimeout(application, 250);
-    } else {
-		// attach to deviceready event, which is fired when phonegap is all good to go.
-		x$(document).on('deviceready', application, false);
-	}
-}
-
-, isReady = false
-
 // shows id passed
-, display = function(id) {
+var display = function(id) {
     x$(["#welcome", "#play", "#deviceInfo"]).each(function(e, i) {
         var display = '#' + x$(e)[0].id === id ? 'block' : 'none';
         x$(e).css({ 'display':display });
     });
-}
+};
 
 // reg a click to [id]_button, displays id (if it exists) and executes callback (if it exists)
-, when = function(id, callback) {
+var when = function(id, callback) {
     var activity = 'click';
     if ('ontouchstart' in document.documentElement) {
        activity = 'touchstart';
@@ -33,12 +20,11 @@ var run = function(application) {
         if (x$(id).length > 0) {
             display(id);
 		} else {
-			//alert('There is no element with ID "'+ id +'" to display as a result of the click on "'+ id +'_button".');
+			console.error('There is no element with ID "'+ id +'" to display as a result of the click on "'+ id +'_button".');
 		}
         if (callback) {
             callback.call(this);
 		}
 		return false;
     });
-}
-;
+};
