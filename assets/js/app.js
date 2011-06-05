c0769911653267e8de0846799602e919a18819a7
Bernhard Hofmann <bhofmann@hotmail.co.uk>
2011-06-05 12:03:22 +0100

    Reducing screen buttons for a simpler UI and more integrated Android app.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,34 +1,36 @@
-var activity = 'click';
-if ('ontouchstart' in document.documentElement) {
-	activity = 'touchstart';
-};
+run(myApp);
 
-// immediately invoked on first run
-var init = (function () {
-})();
+function myApp() {
+	// immediately invoked on first run
+	var init = (function () {
+		// a little inline controller
+		//when('#welcome');
+		when('#play', function () {
+			display('#play');
+		});
+		when('#deviceInfo', function () {
+			display('#deviceInfo');
+		});
+		var activity = 'click';
+		if ('ontouchstart' in document.documentElement) {
+			activity = 'touchstart';
+		};
+		x$('#reset').on(activity, function() {
+			resetLocks();
+		});
 
-// a little inline controller
-//when('#welcome');
-when('#play', function () {
-	display('#play');
-});
-when('.home', function () {
-	display('#welcome');
-});
-x$('#reset').on(activity, function() {
-	resetLocks();
-});
+		document.addEventListener("menubutton", onMenuKeyDown, false);
+		document.addEventListener("searchbutton", onSearchKeyDown, false);
+	})();
 
-document.addEventListener("deviceready", phoneGapReady, false);
-function phoneGapReady() {
-	document.addEventListener("menubutton", onMenuKeyDown, false);
-	document.addEventListener("searchbutton", onSearchKeyDown, false);
-};
-function onMenuKeyDown() {
-	display('#welcome');
-}
-function onSearchKeyDown() {
-	display('#deviceInfo');
+	function phoneGapReady() {
+	};
+	function onMenuKeyDown() {
+		display('#welcome');
+	}
+	function onSearchKeyDown() {
+		display('#deviceInfo');
+	}
 }
 /*
 document.addEventListener("backbutton", onBackKeyDown, false);
--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -13,7 +13,7 @@ var run = function(application) {
 
 // shows id passed
 , display = function(id) {
-    x$(["#welcome", "#play", "#deviceInfo"]).each(function(e, i) {
+    x$('.view').each(function(e, i) {
         var display = '#' + x$(e)[0].id === id ? 'block' : 'none';
         x$(e).css({ 'display':display });
     });
