3b6b07226ad5712d7556f5f8e56799521e347001
Bernhard Hofmann <bhofmann@hotmail.co.uk>
2011-06-05 11:38:11 +0100

    Trying to fix broken event sequence.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,47 @@
+var activity = 'click';
+if ('ontouchstart' in document.documentElement) {
+	activity = 'touchstart';
+};
+
+// immediately invoked on first run
+var init = (function () {
+})();
+
+// a little inline controller
+//when('#welcome');
+when('#play', function () {
+	display('#play');
+});
+when('.home', function () {
+	display('#welcome');
+});
+x$('#reset').on(activity, function() {
+	resetLocks();
+});
+
 document.addEventListener("deviceready", phoneGapReady, false);
-
 function phoneGapReady() {
-	var activity = 'click';
-	if ('ontouchstart' in document.documentElement) {
-		activity = 'touchstart';
-	};
-
-	// a little inline controller
-	when('#play', function () {
-		display('#play');
-	});
-	when('.welcome', function () {
-		display('#welcome');
-	});
-	x$('#reset').on(activity, function() {
-		resetLocks();
-	});
-
 	document.addEventListener("menubutton", onMenuKeyDown, false);
-	function onMenuKeyDown() {
-		display('#welcome');
-	}
-
 	document.addEventListener("searchbutton", onSearchKeyDown, false);
-	function onSearchKeyDown() {
-		display('#deviceInfo');
-	}
-	/*
-	document.addEventListener("backbutton", onBackKeyDown, false);
-	document.addEventListener("menubutton", onMenuKeyDown, false);
-	document.addEventListener("pause", onPause, false);
-	document.addEventListener("resume", onResume, false);
-	function onBackKeyDown() {
-		alert('Back button not supported.');
-	}
-	function onPause() {
-		// Handle the pause event
-	}
-	function onResume() {
-		alert('Welcome back! :D');
-	}
-	*/
+};
+function onMenuKeyDown() {
+	display('#welcome');
 }
+function onSearchKeyDown() {
+	display('#deviceInfo');
+}
+/*
+document.addEventListener("backbutton", onBackKeyDown, false);
+document.addEventListener("menubutton", onMenuKeyDown, false);
+document.addEventListener("pause", onPause, false);
+document.addEventListener("resume", onResume, false);
+function onBackKeyDown() {
+	alert('Back button not supported.');
+}
+function onPause() {
+	// Handle the pause event
+}
+function onResume() {
+	alert('Welcome back! :D');
+}
+*/
--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -1,13 +1,26 @@
+// the app method accepts a fn to invoke on init unobtrusively 
+var run = function(application) {
+    if ((navigator.userAgent.indexOf('Browzr') > -1) || navigator.platform === 'Win32') {
+        // blackberry
+        setTimeout(application, 250);
+    } else {
+		// attach to deviceready event, which is fired when phonegap is all good to go.
+		x$(document).on('deviceready', application, false);
+	}
+}
+
+, isReady = false
+
 // shows id passed
-var display = function(id) {
+, display = function(id) {
     x$(["#welcome", "#play", "#deviceInfo"]).each(function(e, i) {
         var display = '#' + x$(e)[0].id === id ? 'block' : 'none';
         x$(e).css({ 'display':display });
     });
-};
+}
 
 // reg a click to [id]_button, displays id (if it exists) and executes callback (if it exists)
-var when = function(id, callback) {
+, when = function(id, callback) {
     var activity = 'click';
     if ('ontouchstart' in document.documentElement) {
        activity = 'touchstart';
@@ -27,4 +40,5 @@ var when = function(id, callback) {
 		}
 		return false;
     });
-};
+}
+;
